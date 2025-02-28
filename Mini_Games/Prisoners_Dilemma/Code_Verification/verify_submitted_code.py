b567d4862d36ad0e10482fe4e6c2d90e65b757c0
Max <115229630+Max-ErrorMessage@users.noreply.github.com>
2025-02-28 01:20:30 +0000

    I think that last commit got it

--- Mini_Games/Prisoners_Dilemma/Code_Verification/verify_submitted_code.py
+++ Mini_Games/Prisoners_Dilemma/Code_Verification/verify_submitted_code.py
@@ -27,10 +27,6 @@ try:
     exec(file_code, namespace)
 except SyntaxError as se:
     print(f"There is a syntax error in your code in line {se.lineno - 3}")
-    with open(f"/var/www/Mini_Games/Prisoners_Dilemma/Code_Verification/User_Submitted_Code/test_user_{user_id}.txt", 'w') as file:
-        file.write(file_code)
-    with open(f"/var/www/Mini_Games/Prisoners_Dilemma/Code_Verification/User_Submitted_Code/test_user_{user_id}_base_code.txt", 'w') as file:
-        file.write(code)
     quit()
 
 user_function = namespace[f"user_{user_id}"]
