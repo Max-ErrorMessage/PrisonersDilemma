f174c5602242f711386a3f56a264858afee468ea
Max <115229630+Max-ErrorMessage@users.noreply.github.com>
2025-02-28 01:30:54 +0000

    Fixed the simulation

--- Mini_Games/Prisoners_Dilemma/simulate_2_players.py
+++ Mini_Games/Prisoners_Dilemma/simulate_2_players.py
@@ -30,11 +30,11 @@ for i in range(rounds):
         scores[player_1] += 5
         scores[player_2] += 5
     elif player_1_decision and not player_2_decision:  # Player 1 betrayed player 2
-        scores[player_1] += 10
-        scores[player_2] -= 1
-    elif not player_1_decision and player_2_decision:  # Player 2 betrayed player 1
         scores[player_1] -= 1
         scores[player_2] += 10
+    elif not player_1_decision and player_2_decision:  # Player 2 betrayed player 1
+        scores[player_1] += 10
+        scores[player_2] -= 1
 
     # If none of these things happened then they both betrayed one another and scores remain the same
 
