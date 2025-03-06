2bf1a9b74ce4efa97ff62d0567192fbd140633c5
Max <115229630+Max-ErrorMessage@users.noreply.github.com>
2025-03-06 15:56:18 +0000

    Not sure why this was commented out

--- html/newSubmission.php
+++ html/newSubmission.php
@@ -1,14 +1,12 @@
 <?php
 session_start();
 
-// Check if the session variable 'uname' is set
-//if (!isset($_SESSION['uname'])) {
-    // Redirect to login page if 'uname' is not set
-//    header("Location: /signin.php");
-//    exit();
-//}
+if (!isset($_SESSION['uname'])) {
+   header("Location: /signin.php");
+   exit();
+}
 
-// If 'uname' is set, display the welcome message
+
 $uname = htmlspecialchars($_SESSION['uname']);
 if(!isset($_SESSION['code'])){
     $txt = "";
