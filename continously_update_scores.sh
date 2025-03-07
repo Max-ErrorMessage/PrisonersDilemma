f2af725124038165d1cf4f70061973114a3664b5
root <root@srv739220.hstgr.cloud>
2025-03-07 12:49:19 +0000

    Added the sh script to git

---
+++ continously_update_scores.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+while true; do
+    
+    usr/bin/php /var/www/Mini_Games/Prisoners_Dilemma/update_scores.php &>/dev/null
+    
+    echo "Scores updated at $(date)"
+    
+    sleep 10
+done
