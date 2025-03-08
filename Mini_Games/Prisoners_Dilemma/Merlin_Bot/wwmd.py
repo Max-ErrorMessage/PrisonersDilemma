317ebdc531570e0a59264e01d1265c66d2213fc0
Max <115229630+Max-ErrorMessage@users.noreply.github.com>
2025-03-08 15:42:35 +0000

    WWMD updates

--- Mini_Games/Prisoners_Dilemma/Merlin_Bot/wwmd.py
+++ Mini_Games/Prisoners_Dilemma/Merlin_Bot/wwmd.py
@@ -14,14 +14,20 @@ Merlin_Agent = getattr(merlin_module, "AI_Agent", None)
 
 merlin = Merlin_Agent()
 merlin.load_model('/var/www/Mini_Games/Prisoners_Dilemma/Merlin_Bot/merlin.pkl')
-    
+
 
 def read_bin(filename, n):
     with open(filename, "rb") as f:
         byte_data = f.read()
-    
+
     bit_array = np.unpackbits(np.frombuffer(byte_data, dtype=np.uint8))
-    
+
     return bit_array[:n].astype(bool)
 
+
 s = read_bin('/var/www/Mini_Games/Prisoners_Dilemma/Merlin_Bot/queries/self_decisions_{query_id}.bin', n)
+o = read_bin('/var/www/Mini_Games/Prisoners_Dilemma/Merlin_Bot/queries/opponent_decisions_{query_id}.bin', n)
+
+features = merlin.extract_features(s, o)
+
+print("1" if merlin.action(features) else "0")
