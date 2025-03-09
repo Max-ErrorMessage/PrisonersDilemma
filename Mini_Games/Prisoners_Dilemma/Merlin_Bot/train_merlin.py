d709f557bd4b83f3da8095e443c73bf0278808a4
Max <115229630+Max-ErrorMessage@users.noreply.github.com>
2025-03-09 14:20:12 +0000

    Changed the way that merlin trains

--- Mini_Games/Prisoners_Dilemma/Merlin_Bot/train_merlin.py
+++ Mini_Games/Prisoners_Dilemma/Merlin_Bot/train_merlin.py
@@ -1,7 +1,39 @@
 from merlin import AI_Agent
 import importlib.util
 import random
-import numpy as np
+
+def sim_2_players(player_1_function, player_2_function, game_length):
+    player_1_score, player_2_score = 0, 0
+    player_1_decisions, player_2_decisions = [], []
+
+    for _ in range(game_length):
+        player_1_decision = player_1_function(player_1_decisions, player_2_decisions, player_1_decisions,
+                                              player_2_decisions, len(player_1_decisions))
+        player_2_decision = player_2_function(player_1_decisions, player_2_decisions, player_1_decisions,
+                                              player_2_decisions, len(player_1_decisions))
+
+        if player_1_decision and player_2_decision:
+            player_1_score += 5
+            player_2_score += 5
+        elif player_1_decision and not player_2_decision:
+            player_1_score -= 1
+            player_2_score += 10
+        elif not player_1_decision and player_2_decision:
+            player_1_score += 10
+            player_2_score -= 1
+
+        player_1_decisions.append(player_1_decision)
+        player_2_decisions.append(player_2_decision)
+
+    return player_1_score, player_2_score, player_1_decisions, player_2_decisions
+
+def true_bot(self_decisions, opponent_decisions, s, o, n): return True
+def false_bot(self_decisions, opponent_decisions, s, o, n): return False
+def random_bot(self_decisions, opponent_decisions, s, o, n): return random.choice([True, False])
+def copy_bot(self_decisions, opponent_decisions, s, o,n): return True if n == 0 else o[-1]
+def grudge_bot(self_decisions, opponent_decisions, s, o, n): return sum(opponent_decisions) == n
+
+base_strategies = [true_bot, false_bot, random_bot, copy_bot, grudge_bot]
 
 module_path = "/var/www/Mini_Games/Prisoners_Dilemma/Computer_Generated_Files/user_codes.py"
 
@@ -11,71 +43,42 @@ spec.loader.exec_module(user_codes_module)
 
 user_codes = getattr(user_codes_module, "user_code", None)
 
+heuristic_highest_scores = dict({})
+for user_function in user_codes.values():
+    for strategy in base_strategies:
+        user_score, strategy_score, _, _ = sim_2_players(user_function, strategy, 400)
+        if user_function not in heuristic_highest_scores:
+            heuristic_highest_scores[user_function] = strategy_score
+        elif strategy_score > heuristic_highest_scores[user_function]:
+            heuristic_highest_scores[user_function] = strategy_score
+
 merlin = AI_Agent()
 merlin.load_model()
 
 user_codes['0'] = merlin.action
 
-for repeat in range(1000):
+for repeat in range(10000):
     scores = dict({user: 0 for user in user_codes.keys()})
-    merlin_games = []
-    game_length = random.randint(200, 400)
+    game_length = random.randint(400)
     for player_1 in user_codes.keys():
         for player_2 in user_codes.keys():
-            if player_1 <= player_2:
+            if player_1 == player_2 or '0' not in [player_1, player_2]:
                 continue
 
-            player_1_score, player_2_score = 0, 0
-            player_1_decisions, player_2_decisions = [], []
-            player_1_function, player_2_function = user_codes[player_1], user_codes[player_2]
-
-            for _ in range(game_length):
-                player_1_decision = player_1_function(player_1_decisions, player_2_decisions, player_1_decisions,
-                                                      player_2_decisions, len(player_1_decisions))
-                player_2_decision = player_2_function(player_1_decisions, player_2_decisions, player_1_decisions,
-                                                      player_2_decisions, len(player_1_decisions))
-
-                scores_before = player_1_score, player_2_score
-
-                if player_1_decision and player_2_decision:
-                    player_1_score += 5
-                    player_2_score += 5
-                elif player_1_decision and not player_2_decision:
-                    player_1_score -= 1
-                    player_2_score += 10
-                elif not player_1_decision and player_2_decision:
-                    player_1_score += 10
-                    player_2_score -= 1
-
-                player_1_decisions.append(player_1_decision)
-                player_2_decisions.append(player_2_decision)
-
-            scores[player_1] += player_1_score
-            scores[player_2] += player_2_score
+            player_1_score, player_2_score, player_1_decisions, player_2_decisions = sim_2_players(user_codes[player_1],
+                                                                                                   user_codes[player_2],
+                                                                                                   game_length)
 
             if player_2 == '0':
-                merlin_games.append((player_2_decisions, player_1_decisions))
-
-    score_values = np.array(list(scores.values()))
-
-    mean_score = np.mean(score_values)
-    std_dev = np.std(score_values)
-    z_scores = {user_id: (score - mean_score) / std_dev for user_id, score in scores.items()}
-    
-    if max(scores.values()) == scores['0']:
-        reward = 10 # Massively reward first place
-    else:
-        reward = z_scores['0']
-
-    for game in merlin_games:
-        for index, player_1_decision in enumerate(game[0]):
-            state = merlin.extract_features(game[0][:index - 1], game[1][:index - 1])
-            action = player_1_decision
-            next_state = merlin.extract_features(game[0][:index], game[1][:index])
-            merlin.update_q_value(state, action, reward, next_state)
+                reward = heuristic_highest_scores[user_codes[player_1]] / player_2_score
+                for index, (player_1_decision, player_2_decision) in enumerate(zip(player_1_decisions, player_2_decisions)):
+                    state = merlin.extract_features(player_1_decisions[:index - 1], player_2_decisions[:index - 1])
+                    action = player_1_decision
+                    next_state = merlin.extract_features(player_1_decisions[:index], player_2_decisions[:index])
+                    merlin.update_q_value(state, action, reward, next_state)
 
 
-    if repeat % 100 == 0:
-        print(f"Finished simulation {repeat}/1000")
+    if repeat % 1000 == 0:
+        print(f"Finished simulation {repeat}/10000")
 
 merlin.save_model('/var/www/Mini_Games/Prisoners_Dilemma/Merlin_Bot/merlin.pkl')
