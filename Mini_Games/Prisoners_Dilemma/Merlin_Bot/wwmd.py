a54afab67be89773aa382a8992892c42174d8783
Max <115229630+Max-ErrorMessage@users.noreply.github.com>
2025-03-11 19:17:50 +0000

    semi colon forgotten

--- Mini_Games/Prisoners_Dilemma/Merlin_Bot/wwmd.py
+++ Mini_Games/Prisoners_Dilemma/Merlin_Bot/wwmd.py
@@ -4,6 +4,9 @@ import sys
 s = sys.argv[1]
 o = sys.argv[2]
 
+with open("/var/www/Mini_Games/Prisoners_Dilemma/Merlin_Bot/wwmd.log", "w") as file:
+    file.write(f"S: {s}\nO: {o}")
+
 self_decisions = [bit == '1' for bit in s]
 opponent_decisions = [bit == '1' for bit in o]
 
