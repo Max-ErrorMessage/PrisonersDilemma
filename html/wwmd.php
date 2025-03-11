00e7ec7bcdb63806aacf932a10085cd079438f6f
Max <115229630+Max-ErrorMessage@users.noreply.github.com>
2025-03-11 14:31:37 +0000

    Learning php from scratch over here

--- html/wwmd.php
+++ html/wwmd.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION['uname'])) {
     header("Location: /signin.php");
     exit();
 }
-
+$_SESSION['trusts'] = 0;
 $uname = htmlspecialchars($_SESSION['uname']);
 
 // Initialize decisions array in session if not already set
@@ -16,6 +16,7 @@ if (!isset($_SESSION['user_decisions'])) {
 
 if (isset($_POST['decide_true'])) {
     $_SESSION['user_decisions'][] = true;
+    $_SESSION['trusts']++
 }
 ?>
 <html>
@@ -24,9 +25,6 @@ if (isset($_POST['decide_true'])) {
         <link rel="stylesheet" href="profile.css">
         <link rel="icon" href="/t.ico" type="image/x-icon">
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/themes/prism.min.css" rel="stylesheet" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/prism.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-python.min.js"></script>
 
         <title>Twokie - WWMD?</title>
     </head>
@@ -43,7 +41,7 @@ if (isset($_POST['decide_true'])) {
             <h1>What Would Merlin Do?</h1>
         </div>
         <div id="Game">
-            <h1>Trusts: <?php echo count($_SESSION['user_decisions']); ?></h1>
+            <h1>Trusts: <?php echo $_SESSION['user_decisions']; ?></h1>
             <form method="post">
                 <button type="submit" name="decide_true">Trust</button>
             </form>
