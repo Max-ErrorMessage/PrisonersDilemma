1225bec2da08532f23d4566c91f03008e9e639c7
Max <115229630+Max-ErrorMessage@users.noreply.github.com>
2025-03-11 14:05:54 +0000

    WWMD.py

--- html/wwmd.php
+++ html/wwmd.php
@@ -21,7 +21,7 @@ $uname = htmlspecialchars($_SESSION['uname']);
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/prism.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/prism/1.29.0/components/prism-python.min.js"></script>
 
-    	<title>Twokie - My profile</title>
+    	<title>Twokie - WWMD?</title>
 
     </head>
     <body>
@@ -32,95 +32,14 @@ $uname = htmlspecialchars($_SESSION['uname']);
             <a href="profile.php" class="nav-link">My Profile</a>
             <a href="signin.php" id="signinbutton" class="nav-link"><?php echo $uname; ?></a>
         </div>
-        <?php
-            include '../db.php';
-
-            $sql = "
-                SELECT gold, silver, bronze, yg, ys, yb
-                FROM Accounts
-                WHERE Accounts.Username = :username;
-            ";
-
-            $stmt = $pdo->prepare($sql);
-            $stmt->bindParam(":username", $uname);
-            $stmt->execute();
-            $medals = $stmt->fetch(PDO::FETCH_ASSOC);
-
-            $gold = $medals["gold"];
-            $silver = $medals["silver"];
-            $bronze = $medals["bronze"];
-            $yg = $medals["yg"];
-            $ys = $medals["ys"];
-            $yb = $medals["yb"];
-        ?></p>
-        <div id="medals">
-            <div id="medals1">
-                <div id="gold" class="medal">
-                    <img src="images/gold.png"/>
-                    <p><?php echo $gold;?></p>
-                </div>
-                <div id="silver" class="medal">
-                    <img src="images/silver.png"/>
-                    <p><?php echo $silver;?></p>
-                </div>
-                <div id="bronze" class="medal">
-                    <img src="images/bronze.png"/>
-                    <p><?php echo $bronze;?></p>
-                </div>
-            </div>
-            <div id="medals2">
-                <div id="gold2" class="medal">
-                    <img src="images/gold.png"/>
-                    <p><?php echo $yg;?></p>
-                </div>
-                <div id="silver2" class="medal">
-                    <img src="images/silver.png"/>
-                    <p><?php echo $ys;?></p>
-                </div>
-                <div id="bronze2" class="medal">
-                    <img src="images/bronze.png"/>
-                    <p><?php echo $yb;?></p>
-                </div>
-            </div>
-        </div>
         <div id="Main">
             <br><br>
-            <h1>My Profile:</h1>
+            <h1>What Would Merlin Do?</h1>
             <br>
             <p>Username: <?php echo $uname; ?></p>
-            <p>Submissions: <?php
+        </div>
+        <div id="Game">
 
-                                $sql = "
-                                    SELECT Submission.Code, Submission.Game_ID
-                                    FROM Submission
-                                    INNER JOIN Accounts ON Submission.User_ID = Accounts.User_ID
-                                    WHERE Accounts.Username = :username
-				    ORDER BY Submission.Game_ID;
-                                ";
-
-                                $stmt = $pdo->prepare($sql);
-                                $stmt->bindParam(":username", $uname);
-                                $stmt->execute();
-
-                                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                                    $code = $row['Code'];
-                                    $gameid = $row['Game_ID'];
-
-
-                                    if ($gameid == 1){
-                                        echo "<h3>Prisoner's Dilemma:</h3>";
-                                    }
-
-                                    if ($gameid == 2){
-                                        echo "<h3>Yahtzee:</h3>";
-                                        $code = str_replace("$","\n\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n", $code);
-                                    }
-                                    echo "<pre id='example'><code class='language-python'>" . htmlspecialchars($code) . "</code></pre>";
-                                }
-
-                                $pdo = null;
-
-                                ?></p>
         </div>
 
     </body>
