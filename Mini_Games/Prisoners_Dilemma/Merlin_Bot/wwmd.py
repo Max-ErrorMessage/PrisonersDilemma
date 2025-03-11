a3bac73ef85323dec66c119f1c6c098193435e08
Max <115229630+Max-ErrorMessage@users.noreply.github.com>
2025-03-11 19:27:27 +0000

    Fixing bugs

--- Mini_Games/Prisoners_Dilemma/Merlin_Bot/wwmd.py
+++ Mini_Games/Prisoners_Dilemma/Merlin_Bot/wwmd.py
@@ -8,8 +8,6 @@ except IndexError:
     s = ""
     o = ""
 
-print("Cree")
-
 with open("/var/www/Mini_Games/Prisoners_Dilemma/Merlin_Bot/wwmd.log", "w") as file:
     file.write(f"S: {s}\nO: {o}")
 
@@ -19,6 +17,6 @@ opponent_decisions = [bit == '1' for bit in o]
 merlin = AI_Agent(epsilon=0)
 merlin.load_model('/var/www/Mini_Games/Prisoners_Dilemma/Merlin_Bot/merlin.pkl')
 
-choice = merlin.choose_action(self_decisions, opponent_decisions)
+choice = merlin.action(self_decisions, opponent_decisions)
 
-print(1 if choice else 0)
+print(choice)
