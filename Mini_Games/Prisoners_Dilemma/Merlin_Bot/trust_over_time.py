c95fd05a312d215bd034a3845c826fb3f20df69d
root <root@srv739220.hstgr.cloud>
2025-03-13 17:08:20 +0000

    Unknown changes

--- Mini_Games/Prisoners_Dilemma/Merlin_Bot/trust_over_time.py
+++ Mini_Games/Prisoners_Dilemma/Merlin_Bot/trust_over_time.py
@@ -4,11 +4,11 @@ import pandas as pd
 df = pd.read_csv('/var/www/Mini_Games/Prisoners_Dilemma/Merlin_Bot/trustyness')
 
 plt.figure(figsize=(8, 5))
-plt.plot(df.index, df["Total Trusts Scores"], marker='o', color='b', label="Trust")
-plt.plot(df.index, df["Total Betrayal Scores"], marker='s', color='r', label="Betrayal")
+plt.plot(df.index, df["Total Trusts Scores"], color='b', label="Trust")
+plt.plot(df.index, df["Total Betrayal Scores"], color='r', label="Betrayal")
 
 plt.xlabel("Training round")
-plt.ylabel("Total Q-Values")
+plt.ylabel("States where this action is preffered")
 plt.title("Trust and betrayal of Merlin over time")
 plt.legend()
 
