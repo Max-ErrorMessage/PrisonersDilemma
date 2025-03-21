070e22c084df4a76604e0965aeadcdd94143531c
savvyrdruid <savvydruid5@outlook.com>
2025-03-21 20:16:35 +0000

    updating Yatzee submission page

--- html/newYahtzeeSubmission.php
+++ html/newYahtzeeSubmission.php
@@ -49,7 +49,7 @@ $uname = htmlspecialchars($_SESSION['uname']);
 <code>
 <div id="title">Yahtzee:</div>
 <body>
-<div id="NavBar">
+    <div id="NavBar">
         <img src="images/twokielogo.png" id="navbarLogo">
         <a href="index.php" class="nav-link">Home</a>
         <a href="leaderboards.php" class="nav-link">Leaderboards</a>
@@ -57,53 +57,54 @@ $uname = htmlspecialchars($_SESSION['uname']);
         <a href="profile.php" class="nav-link">My Profile</a>
         <a href="signin.php" id="signinbutton" class="nav-link"><?php echo $uname; ?></a>
     </div>
-
-    <form action="submission.php" method="POST">
-        <label for="name">Enter your reroll function!:</label>
-        <textarea id="name" name="code" is="highlighted-code" placeholder="return []   #reroll no dice" onkeydown="return stopTab(event);" required></textarea>
-        <label for="name">Enter your select function!:</label>
-        <textarea id="name2" name="code2" is="highlighted-code" placeholder="return choices[0]   #returns first available move" onkeydown="return stopTab(event);" required></textarea>
-        <input type="hidden" name="game_id" value=2>
-        <button type="submit" name="submitCode" value="submit">Submit</button>
-    </form>
-
-    <div class="box" id="php"><?php
-        session_start();
-        if(isset($_SESSION['Error3'])){
-            echo "<h2>" . $_SESSION['Error3'] . "</h2>";
-        } 
-    ?></div>
+    <div class="container">
+        <div class="box">
+            <h1>Your task:</h1>
+            <p>First box: reroll function<br>
+            &emsp;     - This function will be called when you need to reroll dice<br>
+            &emsp;     - It should return a list of the dice you want to reroll<br>
+            &emsp;     - It should return a list of the dice you want to reroll<br>
+            &emspl     - The list should contain the indexes of the dice you want to reroll</p>
+            <p>Second box: select function<br>
+            &emsp;     - This function will be called when you need to select a move after rolling<br>
+            &emsp;     - It should return the move you want to make</p>
+        </div>
+        <div class="box">
+            <h1>Variables:</h1>
+            <div id="var">dice:</div>
+            <p> &emsp;     - an array of your current dice</p>
+            <div id="var">availability:</div>
+            <p> &emsp;     - a dictionary containing the availability of each move<br>
+            (key is the move, value is a boolean)</p>
+            <div id="var">available_points:</div>
+            <p> &emsp;     - a dictionary containing the amount of points each move would score</p>
+            <div id="var">claimed_points:</div>
+            <p> &emsp;     - a dictionary containing the amount of points each scored move<br>
+            (key is the move, value is the points)<br>
+            list of keys:<br>
+            &emsp;      - "Yahtzee", "Large Straight", "Full House", "Small Straight", "4 of a Kind", <br>
+            &emsp;      "3 of a Kind", "Sixes", "Fives", "Fours", "Threes", "Twos", "Ones", "Chance"</p>
+        </div>
+    </div>
+    <div class="container">
+        <div class="box2">
+            <form action="submission.php" method="POST">
+                <label for="name">Enter your reroll function!:</label>
+                <textarea id="name" name="code" is="highlighted-code" placeholder="return []   #reroll no dice" onkeydown="return stopTab(event);" required></textarea>
+                <label for="name">Enter your select function!:</label>
+                <textarea id="name2" name="code2" is="highlighted-code" placeholder="return choices[0]   #returns first available move" onkeydown="return stopTab(event);" required></textarea>
+                <input type="hidden" name="game_id" value=2>
+                <button type="submit" name="submitCode" value="submit">Submit</button>
+            </form>
+        </div>
+        <div class="box" id="php"><?php
+            session_start();
+            if(isset($_SESSION['Error3'])){
+                echo "<h2>" . $_SESSION['Error3'] . "</h2>";
+            } 
+        ?></div>
+    </div>
 </body>
-<div id="info">
-    <h1>Your task:</h1>
-    <pre>
-First box: reroll function
-    - This function will be called when you need to reroll dice
-    - It should return a list of the dice you want to reroll
-    - The list should contain the indexes of the dice you want to reroll
-
-Second box: select function
-    - This function will be called when you need to select a move after rolling
-    - It should return the move you want to make
-    </pre>
-    <h1>
-        Variables:
-    </h1>
-    <div id="var">dice:</div>
-    <pre>   - an array of your current dice</pre>
-    <div id="var">availability:</div>
-    <pre>   - a dictionary containing the availability of each move
-    (key is the move, value is a boolean)</pre>
-    <div id="var">available_points:</div>
-    <pre>   - a dictionary containing the amount of points each move would score
-    <div id="var">claimed_points:</div>
-    <pre>   - a dictionary containing the amount of points each scored move
-    (key is the move, value is the points)</pre>
-    <pre>
-list of keys:
-    - "Yahtzee", "Large Straight", "Full House", "Small Straight", "4 of a Kind", 
-    "3 of a Kind", "Sixes", "Fives", "Fours", "Threes", "Twos", "Ones", "Chance"
-    </pre>
     <h1>
         Example bot:
     </h1>
