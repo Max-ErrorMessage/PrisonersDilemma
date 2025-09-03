40cdafa39c4b1e1d982a1bab08cf8f01cc7f02d7
james <james2004j.a@gmail.com>
2025-09-03 23:05:05 +0100

    rebuilding with bootstrap

--- html/Unres/Match.php
+++ html/Unres/Match.php
@@ -124,6 +124,8 @@ $decks = $stmt->fetchAll(PDO::FETCH_ASSOC);
             <div class="form-group"><button class="btn btn-primary btn-block" type="submit">Submit</button></div>
         </form>
     </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
