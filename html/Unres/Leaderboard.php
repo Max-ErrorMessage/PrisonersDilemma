50e36296fa7563409d2cd2572fd304ee7a9de73e
james <james2004j.a@gmail.com>
2025-09-04 23:36:50 +0100

    lb page

--- html/Unres/Leaderboard.php
+++ html/Unres/Leaderboard.php
@@ -35,6 +35,7 @@ FROM
 	LEFT JOIN colours_of_cards coc ON cid.card_id = coc.card_id
 	WHERE coc.colour_id IS NOT NULL
 	AND coc.identity = 1
+	AND coc.card_id not in (93,113,142,150,180)
 	GROUP BY cid.deck_id, coc.colour_id
 ) as t1
 inner join decks d on d.id = t1.deck
