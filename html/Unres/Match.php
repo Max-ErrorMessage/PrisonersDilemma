5c1349433393abb148bf9461ad28e6294b235887
GaryFallova <james2004j.a@gmail.com>
2025-09-06 19:52:43 +0100

    fixing lb order

--- html/Unres/Match.php
+++ html/Unres/Match.php
@@ -7,7 +7,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-include "/var/www/unresdb.php";
+include "/var/www/html/Unres/db.php";
 
 // Fetch all decks
 $stmt = $pdo->query("SELECT id, provided_archetype, elo FROM decks");
