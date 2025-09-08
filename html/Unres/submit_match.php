cf001ee9c8435e36d8e1a40483c73325056ba682
james <james2004j.a@gmail.com>
2025-09-08 21:36:25 +0100

    submit_match.php

--- html/Unres/submit_match.php
+++ html/Unres/submit_match.php
@@ -5,11 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 
-    $command = "/usr/bin/python3 /var/www/Unres-Meta/elo/update_elo.py $winner_id $loser_id 2>&1";
-    $output1 = shell_exec($command);
-    //file_put_contents("/var/www/html/Unres/php_python_debug.log", $output . PHP_EOL, FILE_APPEND);
-
-    $command = "cd /var/www/Unres-Meta/elo && /usr/bin/python3 update_elo.py $winner_id $loser_id 2>&1";
+    $command = "/var/www/Unres-Meta/elo/venv/bin/python /var/www/Unres-Meta/elo/update_elo.py $winner_id $loser_id 2>&1";
     $output2 = shell_exec($command);
     //file_put_contents("/var/www/html/Unres/php_python_debug.log", date('c') . " - CMD: $command\nOUTPUT:\n" . $output . "\n", FILE_APPEND);
 
