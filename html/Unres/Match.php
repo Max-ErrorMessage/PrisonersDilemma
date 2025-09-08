e2ece7444a849db665fd576dfa3ac50b66d54e6b
james <james2004j.a@gmail.com>
2025-09-08 22:10:09 +0100

    nicer match submission

--- html/Unres/Match.php
+++ html/Unres/Match.php
@@ -157,10 +157,10 @@ $decks = $stmt->fetchAll(PDO::FETCH_ASSOC);
           const y = e.clientY / height;
 
           // Map 0-1 to background-position offsets (-10% to +10%)
-          const offsetX = (x - 0.5) * 10; // -10% to +10%
-          const offsetY = (y - 0.5) * 10;
+          const offsetX = (x - 0.5) * 5; // -10% to +10%
+          const offsetY = (y - 0.5) * 5;
 
-          div.style.backgroundPosition = `${50 + offsetX}% ${30 + offsetY}%`;
+          div.style.backgroundPosition = `${50 + offsetX}% ${15 + offsetY}%`;
         });
     </script>
 
