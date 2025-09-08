15c0446cf45494dfc64f1d91eb009c4cf1cf25ef
james <james2004j.a@gmail.com>
2025-09-08 20:16:15 +0100

    submit_match.php

--- html/Unres/submit_match.php
+++ html/Unres/submit_match.php
@@ -5,15 +5,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 
-    $command = "cd /var/www/Unres-Meta/elo && python3 update_elo.py $winner_id $loser_id";
-    $output = shell_exec($command);
-    file_put_contents("/var/www/html/Unres/php_python_debug.log", $output . PHP_EOL, FILE_APPEND);
+    $command = "python3 /var/www/Unres-Meta/elo/update_elo.py $winner_id $loser_id 2>&1";
+    $output1 = shell_exec($command);
+    //file_put_contents("/var/www/html/Unres/php_python_debug.log", $output . PHP_EOL, FILE_APPEND);
 
     $command = "cd /var/www/Unres-Meta/elo && /usr/bin/python3 update_elo.py $winner_id $loser_id 2>&1";
-    $output = shell_exec($command);
-    file_put_contents("/var/www/html/Unres/php_python_debug.log", date('c') . " - CMD: $command\nOUTPUT:\n" . $output . "\n", FILE_APPEND);
+    $output2 = shell_exec($command);
+    //file_put_contents("/var/www/html/Unres/php_python_debug.log", date('c') . " - CMD: $command\nOUTPUT:\n" . $output . "\n", FILE_APPEND);
 
     header("Location: Leaderboard.php");
+    echo $output1;
+    echo $output2;
     exit();
 }
 ?>
