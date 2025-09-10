44ed9dc21e10e9ca50d8750454fba1825ac7a292
james <james2004j.a@gmail.com>
2025-09-10 21:23:32 +0100

    leaderboard backgrounds

--- html/Unres/Leaderboard.php
+++ html/Unres/Leaderboard.php
@@ -14,25 +14,36 @@ include "/var/www/html/Unres/db.php";
 
 // Fetch all decks
 $stmt = $pdo->query('SELECT
-        cod.deck_id as id,
-        SUM(
-                CASE cod.colour_id
-                        WHEN 1 THEN 1
-                        WHEN 2 THEN 2
-                        WHEN 3 THEN 4
-                        WHEN 4 THEN 8
-                        WHEN 5 THEN 16
-                        ELSE 0
-                END
-        ) AS colour,
-	d.name as name,
-	d.custom_id as cid,
-        d.elo AS elo,
-        d.provided_archetype AS arch
-FROM colours_of_decks cod
-INNER JOIN decks d ON d.id = cod.deck_id
-GROUP BY d.id
-ORDER BY elo;
+	t1.deck as id,
+	SUM(
+		CASE t1.colour
+            		WHEN 1 THEN 1
+            		WHEN 2 THEN 2
+            		WHEN 3 THEN 4
+            		WHEN 4 THEN 8
+            		WHEN 5 THEN 16
+            		ELSE 0
+        	END
+	) AS colour,
+	d.elo as elo,
+	d.provided_archetype as arch,
+	d.name as cust_id
+FROM
+(
+	SELECT
+		cid.deck_id AS deck,
+		coc.colour_id AS colour,
+		count(cid.card_id) as n
+	FROM card_in_deck cid
+	LEFT JOIN colours_of_cards coc ON cid.card_id = coc.card_id
+	AND coc.identity = 1
+	AND coc.card_id not in (93,113,142,150,180,249)
+	GROUP BY cid.deck_id, coc.colour_id
+) as t1
+inner join decks d on d.id = t1.deck
+where t1.n > 2
+GROUP BY deck
+ORDER BY elo DESC;
 ');
 $decks = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $rank = 1;
@@ -159,23 +170,6 @@ $rank = 1;
 </head>
 <body>
 
-<<<<<<< HEAD
-    <div class="bg-img">
-        <div id="lb">
-            <div class="illustration"><img src="https://cdn-icons-png.flaticon.com/128/5200/5200866.png"/></div>
-            <br>
-            <table>
-            <?php foreach ($decks as $deck): ?>
-                <tr>
-                    <td>
-                        <div class="n c<?= $rank?>"><span id="r<?= $rank?>"><?= $rank?>.</span></div>
-                    </td><td>
-                        <?php $imageUrl = "images/".$deck['colour'].".png"; ?>
-                        <img class="lbimg" src="<?= htmlspecialchars($imageUrl) ?>" alt="color">
-                    </td><td>
-                        <?= htmlspecialchars($deck['name']) ?><br><span style="color:#aaa;font-family: 'JetBrains Mono', 'IBM Plex Mono', 'Source Code Pro', monospace;">#<?= $deck['cid'] ?></span>
-                    </td><td>
-=======
     <div class="bg-bg">
         <div class="bg-img">
             <div id="lb">
@@ -192,7 +186,6 @@ $rank = 1;
                         </td><td>
                             <?= htmlspecialchars($deck['arch']) ?><br><span style="color:#aaa;font-family: 'JetBrains Mono', 'IBM Plex Mono', 'Source Code Pro', monospace;">#<?= $deck['cust_id'] ?></span>
                         </td><td>
->>>>>>> a3b45fa3377a871c206b9c34f1eda93c938ada5b
 
                             <div class="ra"><?= htmlspecialchars($deck['elo']) ?></div>
                         </td>
