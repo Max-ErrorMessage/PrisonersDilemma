c865620fb34d67a7d41d8a0cb02dd90de9cf8408
james <james2004j.a@gmail.com>
2025-09-11 22:22:23 +0100

    deck page

--- html/Unres/Leaderboard.php
+++ html/Unres/Leaderboard.php
@@ -168,6 +168,10 @@ $rank = 1;
 
         tr:hover{
             background-color:#345
+            transition:background-color 0.1s
+        }
+        tr{
+            border-radius:5px;
         }
 
     </style>
@@ -182,7 +186,7 @@ $rank = 1;
                     <br>
                     <table>
                     <?php foreach ($decks as $deck): ?>
-                        <tr onclick=<?= '"deck.php?id='.$deck['id'].'"'?>>
+                        <tr data-href=<?= '"deck.php?id='.$deck['id'].'"'?>>
                             <td>
                                 <div class="n c<?= $rank?>"><span id="r<?= $rank?>"><?= $rank?>.</span></div>
                             </td><td>
