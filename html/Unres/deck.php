c81ec8ba8fdc6a3fac97bcaefbc8e4bcc918cceb
james <james2004j.a@gmail.com>
2025-09-13 15:27:45 +0100

    deck page

--- html/Unres/deck.php
+++ html/Unres/deck.php
@@ -27,6 +27,8 @@ foreach ($decks as $d) {
     $deck = $d;
 }
 
+$embed_url = str_replace("/decks/", "/embed/", $deck['decklist_url']);
+
 $stmt = $pdo->prepare('SELECT id, decklist_url, ELO, provided_archetype, name
 FROM decks
 WHERE id = :id;
@@ -54,14 +56,23 @@ $stmt->bindParam(':id',$id, PDO::PARAM_INT);
 $stmt->execute();
 $sb_cards = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-$stmt = $pdo->prepare('SELECT elo_change FROM elo_changes
-WHERE deck_id = :id');
+$stmt = $pdo->prepare('SELECT ec.elo_change, w.name AS winner, l.name AS loser FROM elo_changes ec
+INNER JOIN matches m ON ec.match_id = m.id
+LEFT JOIN decks w ON w.id = m.winner_id
+LEFT JOIN decks l ON l.id = m.loser_id
+WHERE ec.deck_id = :id;');
 $stmt->bindParam(':id',$id, PDO::PARAM_INT);
 $stmt->execute();
 
 $elo = [];
+$winners = [];
+$losers = [];
+$elo_rows = [];
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    $elo[] = (int)$row['elo_change']; // force integer
+    $elo[] = (int)$row['elo_change'];
+    $winners[] = $row['winner'];
+    $losers[] = $row['loser'];
+    $elo_rows[] = $row;
 }
 
 
@@ -79,7 +90,6 @@ foreach ($decks as $d) {
     <meta name="description" content="Create bots to compete in fun minigames! :)">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
     <style>
         .bg-bg, .bg-img, .bg-fg {
           height:100%;
@@ -126,51 +136,37 @@ foreach ($decks as $d) {
         }
 
         #lb {
-          min-width:320px;
-          width:40%;
-          background-color:#1e2833;
-          padding:40px;
-          border-radius:4px;
-          transform:translate(-50%, -50%);
-          position:absolute;
-          top:50%;
-          left:50%;
-          color:#fff;
-          box-shadow:3px 3px 4px rgba(0,0,0,0.2);
-          height:80%;
-          overflow-y:scroll;
+            min-width: 320px;
+            width: 40%;
+            background-color: #1e2833;
+            padding: 40px;
+            border-radius: 4px;
+            transform: translate(-50%, -50%);
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            color: #fff;
+            box-shadow: 3px 3px 4px rgba(0, 0, 0, 0.2);
+            height: 70%;
+            overflow-y: scroll;
         }
 
         ::-webkit-scrollbar{
             display:none;
         }
 
-        .login-dark .illustration {
-          text-align:center;
-        }
-        .illustration img{
-            width:65px;
-        }
+
         td{
             color:#fff;
         }
 
-        table{
-            text-align:left;
-            font-size:15px;
-            width:80%;
-            position:absolute;
-            left:10%;
-        }
 
-        .lbimg{
-            width:25px;
-        }
+
 
     #mb ,#sb{
         width:45%;
         position:absolute;
-        top:25%
+        top:15%
     }
 
     #mb{
@@ -201,6 +197,62 @@ foreach ($decks as $d) {
         filter:  brightness(1.41) saturate(0.55);
     }
 
+    .tab{
+        background-image: linear-gradient(to top, #101820, #1e2833);
+        position: absolute;
+        bottom: 84.9%;
+        height: 50px;
+        width: 80px;
+        border-radius: 5px 5px 0px 0px;
+    }
+
+    .tab img{
+        width: 40px;
+        height: auto;
+        text-align: center;
+        margin: auto;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+    }
+
+    #t1{
+        background-color:#1e2833;
+        background-image:none;
+        left: 31%;
+    }
+    #t2{
+        left: calc(32% + 80px);
+    }
+    #t3{
+        left: calc(33% + 160px);
+    }
+
+    #match-table{
+        left:15%;
+        width:70%;
+        position:absolute;
+        height:30%;
+        overflow-y:scroll;
+        border:1px white solid;
+    }
+    #match-table tr{
+        border-bottom: 1px #555 solid;
+    }
+
+    #match-table table{
+        width:100%;
+    }
+
+    #match-table p{
+        margin:auto;
+    }
+
+    #match-table td{
+        padding:4px;
+    }
+
     </style>
 </head>
 <body>
@@ -210,30 +262,61 @@ foreach ($decks as $d) {
                 <a href="Leaderboard.php" id="back">
                     <img src="https://cdn-icons-png.flaticon.com/128/9795/9795832.png">
                 </a>
+                <a class="tab" id="t1" onclick="switchTab(1)">
+                    <img src="https://cdn-icons-png.flaticon.com/128/6831/6831865.png"/>
+                </a>
+                <a class="tab" id="t2" onclick="switchTab(2)">
+                    <img src="https://cdn-icons-png.flaticon.com/128/9874/9874735.png"/>
+                </a>
+                <a class="tab" id="t3" onclick="switchTab(3)" style="display:none">
+                    <img src="https://cdn-icons-png.flaticon.com/128/3790/3790171.png"/>
+                </a>
                 <div id="lb">
-                    <div class="illustration"><img src="https://cdn-icons-png.flaticon.com/128/9874/9874735.png"/></div>
-                    <br>
-                    <h3 style="text-align:center;"> <?= $deck['name'] ?> </h3>
-                    <div id="mb">
-                        <strong>Mainboard:</strong>
-                        <?php foreach ($mb_cards as $card): ?>
-                        <div style="justify-content:space-between;display:flex; width:100%;">
-                            <span><?= htmlspecialchars($card['name']) ?></span>
-                            <span><?= htmlspecialchars($card['n']) ?></span>
+                    <div id = "page1">
+                        <h3 style="text-align:center;"> <?= $deck['name'] ?> </h3>
+                        <div id="mb">
+                            <strong>Mainboard:</strong>
+                            <?php foreach ($mb_cards as $card): ?>
+                            <div style="justify-content:space-between;display:flex; width:100%;">
+                                <span><?= htmlspecialchars($card['name']) ?></span>
+                                <span><?= htmlspecialchars($card['n']) ?></span>
+                            </div>
+                            <?php endforeach; ?>
                         </div>
-                        <?php endforeach; ?>
+                        <br>
+                        <div id="sb">
+                            <strong>Sideboard:</strong>
+                            <?php foreach ($sb_cards as $card): ?>
+                            <div style="justify-content:space-between;display:flex; width:100%">
+                                <span><?= htmlspecialchars($card['name']) ?></span>
+                                <span><?= htmlspecialchars($card['n']) ?></span>
+                            </div>
+                            <?php endforeach; ?>
+                            <br><br>
+                            <a style="color:#ccc;" href= <?= '"'.$deck['decklist_url'].'"' ?> >Click here for the deck page</a>
+                        </div>
                     </div>
-                    <br>
-                    <div id="sb">
-                        <strong>Sideboard:</strong>
-                        <?php foreach ($sb_cards as $card): ?>
-                        <div style="justify-content:space-between;display:flex; width:100%">
-                            <span><?= htmlspecialchars($card['name']) ?></span>
-                            <span><?= htmlspecialchars($card['n']) ?></span>
+                    <div id="page2" style="display:none">
+                        <h3 style="text-align:center;"> <?= $deck['name'] ?> </h3>
+                        <canvas id="elograph"></canvas>
+                        <div id="match-table">
+                            <table>
+                                <?php foreach ($elo_rows as $row): ?>
+                                    <tr onclick=goToDeck(<?= $deck['id']?>)>
+                                        <td>
+                                            <p><?= htmlspecialchars($row['winner'])?> beat <?= htmlspecialchars($row['loser'])?></p>
+                                        </td><td>
+                                            <p><?= htmlspecialchars($row['elo_change'])?> elo gain</p>
+                                        </td>
+                                    </tr>
+                                    <?php $rank++; ?>
+                                <?php endforeach; ?>
+                            </table>
                         </div>
-                        <?php endforeach; ?>
-                        <br><br>
-                        <a style="color:#ccc;" href= <?= '"'.$deck['decklist_url'].'"' ?> >Click here for the deck page</a>
+                    </div>
+                    <div id="page3" style="display:none">
+                        <h3 style="text-align:center;"> <?= $deck['name'] ?> </h3>
+                        <iframe src=<?='"'.htmlspecialchars($embed_url) . '/primer"'?> width="80%" height="80%"></iframe>
                     </div>
 
                </div>
@@ -242,6 +325,7 @@ foreach ($decks as $d) {
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
         const div2 = document.querySelector('.bg-img');
 
@@ -264,13 +348,98 @@ foreach ($decks as $d) {
         });
 
         const elo_changes = <?php echo json_encode($elo); ?>;
+        const winners = <?php echo json_encode($winners); ?>;
+        const losers = <?php echo json_encode($losers); ?>;
 
         console.log(elo_changes);
         elo_arr = [1000]
         for (let i = 0; i < elo_changes.length; i++) {
           elo_arr.push(elo_arr[elo_arr.length - 1] + elo_changes[i]);
         }
-        console.log(elo_arr)
+
+        labels = ["Starting Elo"]
+        for (var i = 0; i < winners.length; i++){
+            labels.push(winners[i] + " beat " + losers[i])
+        }
+        console.log(winners)
+        console.log(labels)
+        function switchTab(n){
+            tab1 = document.getElementById("t1")
+            tab2 = document.getElementById("t2")
+            tab3 = document.getElementById("t3")
+            page1 = document.getElementById("page1")
+            page2 = document.getElementById("page2")
+            page3 = document.getElementById("page3")
+
+            if (n==1){
+                tab1.style.backgroundColor = "#1e2833"
+                tab1.style.backgroundImage = "none"
+                tab2.style.backgroundImage = "linear-gradient(to top, #101820, #1e2833)"
+                tab2.style.backgroundColor = "none"
+                tab3.style.backgroundImage = "linear-gradient(to top, #101820, #1e2833)"
+                tab3.style.backgroundColor = "#none"
+
+                page1.style.display = "block"
+                page2.style.display = "none"
+                page3.style.display = "none"
+
+            } else if (n==2){
+                tab2.style.backgroundColor = "#1e2833"
+                tab2.style.backgroundImage = "none"
+                tab1.style.backgroundImage = "linear-gradient(to top, #101820, #1e2833)"
+                tab1.style.backgroundColor = "none"
+                tab3.style.backgroundImage = "linear-gradient(to top, #101820, #1e2833)"
+                tab3.style.backgroundColor = "#none"
+
+                page2.style.display = "block"
+                page1.style.display = "none"
+                page3.style.display = "none"
+
+                new Chart(document.getElementById("elograph"), {
+                    type: "line",
+                    data: {
+                      labels: labels,
+                      datasets: [{
+                        label: "Elo Over Time",
+                        data: elo_arr,
+                        borderColor: "White",
+                        fill: true,
+                        backgroundColor: "rgba(255,255,255,0.2)"
+                      }]
+                    },
+                    options: {
+                      scales: {
+                        x: {
+                          ticks: { color: "#ddd" },
+                          grid: { color: "#444" },
+                          display:false
+                        },
+                        y: {
+                          ticks: { color: "#ddd" },
+                          grid: { color: "#444" }
+                        }
+                      },
+                      plugins: {
+                        legend: {
+                          labels: { color: "#ddd" }
+                        }
+                      }
+                    }
+                  });
+
+            }  else if (n==3){
+                tab3.style.backgroundColor = "#1e2833"
+                tab3.style.backgroundImage = "none"
+                tab1.style.backgroundImage = "linear-gradient(to top, #101820, #1e2833)"
+                tab1.style.backgroundColor = "none"
+                tab2.style.backgroundImage = "linear-gradient(to top, #101820, #1e2833)"
+                tab2.style.backgroundColor = "#none"
+
+                page3.style.display = "block"
+                page1.style.display = "none"
+                page2.style.display = "none"
+            }
+        }
     </script>
 </body>
 </html>
