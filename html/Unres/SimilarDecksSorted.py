e9d4e96f4335c3df292cde3429465fe05ad8a8b4
james <james2004j.a@gmail.com>
2025-09-13 16:54:33 +0100

    similarity test

--- html/Unres/SimilarDecksSorted.py
+++ html/Unres/SimilarDecksSorted.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('../../Unres-Meta/elo')  # path to the folder containing similarity_from_matrix.py
 
-import similarity_from_matrix
+import similarity_from_matrix.py
 
 print(deck_similarity(sys.argv[1], sys.argv[2]))
