79b81ba058a9eb86e26e22706fc130e2ba9b17f8
james <james2004j.a@gmail.com>
2025-09-13 17:37:19 +0100

    similarity test

--- html/Unres/SimilarDecksSorted.py
+++ html/Unres/SimilarDecksSorted.py
@@ -1,9 +1,30 @@
 import sys
 from pathlib import Path
 
-# Add the module path
 sys.path.append(str(Path(__file__).parent / "../../Unres-Meta/elo"))
 
 import similarity_from_matrix
+import importlib.util
 
-print(similarity_from_matrix.deck_similarity(455,456))
+def deck_ids():
+  spec = importlib.util.spec_from_file_location("connect_to_db", "/var/www/Unres-Meta/db/connect_to_db.py")
+  connect_to_db = importlib.util.module_from_spec(spec)
+  sys.modules["connect_to_db"] = connect_to_db
+  spec.loader.exec_module(connect_to_db)
+
+  cursor = connect_to_db.cursor
+  conn = connect_to_db.conn
+
+  deck_ids = dict()
+
+  cursor.execute(f"SELECT id, name, url FROM decks")
+  rows = cursor.fetchall()
+
+  for deck_id, url, name in rows:
+    deck_ids[deck_id] = (name, url)
+
+  return deck_ids
+
+ids = deck_ids()
+for d_id in ids:
+    print(str(ids[d_id]) + ": " + str(similarity_from_matrix.deck_similarity(sys.argv[1],id)))
