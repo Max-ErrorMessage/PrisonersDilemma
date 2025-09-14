4f5eeaff0860bf29cff9d8a63e3f55d3736fdf69
GaryFallova <james2004j.a@gmail.com>
2025-09-14 23:17:29 +0100

    deck things

--- html/Unres/decktest.php
+++ html/Unres/decktest.php
@@ -435,7 +435,7 @@ if (count($sim_rows) > 0) {
             </div>
         </div>
     </div>
-    <textarea style="display:none" id="decklist"><?= addslashes($decklist)?></textarea>
+    <textarea readonly style="position:absolute; left:-9999px;" id="decklist"><?= addslashes($decklist)?></textarea>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
