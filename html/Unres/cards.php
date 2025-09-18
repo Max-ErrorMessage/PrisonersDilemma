e86bd269fed03c2e3bef17d9c9a89561a602085e
GaryFallova <james2004j.a@gmail.com>
2025-09-18 03:59:56 +0100

    deck things

--- html/Unres/cards.php
+++ html/Unres/cards.php
@@ -220,7 +220,7 @@ $rank = 1;
                             <td>
                                 <div class="n c<?= $rank?>"><span id="r<?= $rank?>"><?= $rank?>.</span></div>
                             </td><td>
-                                <img src="<?= htmlspecialchars($card['image_url']) ?>">
+                                <img src="<?= htmlspecialchars($card['image_url']) ?>" style = "width:40%">
                             </td><td>
                                 <?= htmlspecialchars($card['card_name']) ?>
                             </td><td>
