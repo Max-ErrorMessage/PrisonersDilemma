c25e466c9ef1ae63db4899341f917a5d7c77adb6
GaryFallova <james2004j.a@gmail.com>
2025-09-19 23:25:20 +0100

    lb graph

--- html/Unres/leaderboardtest.php
+++ html/Unres/leaderboardtest.php
@@ -292,7 +292,7 @@ $arch_output = shell_exec($command);
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
     <script>
 
-        const archetypes = JSON.parse('<?php echo $arch_output; ?>');
+        const archetypes = JSON.parse('<?php echo trim($arch_output); ?>');
         console.log(archetypes);
 
 
