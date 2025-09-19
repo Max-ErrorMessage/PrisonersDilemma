e751749dcc7632ce39435c86c2346f92c2ae0bca
GaryFallova <james2004j.a@gmail.com>
2025-09-19 23:22:45 +0100

    lb graph

--- html/Unres/leaderboardtest.php
+++ html/Unres/leaderboardtest.php
@@ -42,7 +42,6 @@ $pythonScript = 'elo_changes_by_archetype.py';
 $command = 'cd /var/www/Unres-Meta/db && ' . escapeshellcmd($venvPython) . ' ' . ($pythonScript) . ' ' . ' 2>&1';
 $arch_output = shell_exec($command);
 
-echo($sim_output);
 
 ?>
 
