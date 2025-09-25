bcfe5dd8e7d539efc8b03d6da1ded0e50c80ba55
james <james2004j.a@gmail.com>
2025-09-25 21:41:48 +0100

    card graph

--- html/Unres/unresLeaderboardAsATrainDepartureBoard/page.php
+++ html/Unres/unresLeaderboardAsATrainDepartureBoard/page.php
@@ -39,6 +39,12 @@
     }
 
     .overlay::after{
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
         background-image:
         linear-gradient(to bottom, rgba(0,0,0,0.2), transparent)
     }
