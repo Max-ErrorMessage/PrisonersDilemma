354ab5ec28684b94fbf5c0d14a3a7474c6c0e913
james <james2004j.a@gmail.com>
2025-09-25 21:53:41 +0100

    card graph

--- html/Unres/unresLeaderboardAsATrainDepartureBoard/page.php
+++ html/Unres/unresLeaderboardAsATrainDepartureBoard/page.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <link href="https://fonts.googleapis.com/css2?family=VT323&display=swap" rel="stylesheet">
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Unres Leaderboard as a Train Station Departure Board</title>
