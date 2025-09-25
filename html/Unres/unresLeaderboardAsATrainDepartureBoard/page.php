bfff2a8ffa6322ca261126db363e6508fd81c9cd
james <james2004j.a@gmail.com>
2025-09-25 18:10:51 +0100

    card graph

---
+++ html/Unres/unresLeaderboardAsATrainDepartureBoard/page.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Unres Leaderboard as a Train Departure Board</title>
+        <style>
+            #bg{
+                width: 100%;
+                height: 100%;
+                background_image: bg.jpg;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="bg"></div>
+    </body>
+</html>
