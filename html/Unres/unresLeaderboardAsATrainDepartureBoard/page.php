f3302d3b86d4eea1157d39dca1e852978a440b61
james <james2004j.a@gmail.com>
2025-09-25 21:43:45 +0100

    card graph

--- html/Unres/unresLeaderboardAsATrainDepartureBoard/page.php
+++ html/Unres/unresLeaderboardAsATrainDepartureBoard/page.php
@@ -39,7 +39,17 @@
         background-size: 10px 6px;
     }
 
-
+    .overlay::after{
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-image:
+        linear-gradient(to bottom, rgba(0,0,0,0.2), transparent),
+        linear-gradient(to left, rgba(0,0,0,0.2), transparent),
+    }
 
   </style>
 </head>
