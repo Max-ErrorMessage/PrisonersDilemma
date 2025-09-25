16a2fba1fad7b9f33ba8fcbc3667cd6c7b293741
james <james2004j.a@gmail.com>
2025-09-25 17:03:22 +0100

    card graph

--- html/Unres/cards.php
+++ html/Unres/cards.php
@@ -303,6 +303,7 @@ $sbrank = 1;
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script>
         const div2 = document.querySelector('.bg-img');
 
