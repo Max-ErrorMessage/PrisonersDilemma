a5a783bba9925458243a3e3ae724d7349b678050
james <james2004j.a@gmail.com>
2025-09-25 21:44:53 +0100

    card graph

--- html/Unres/unresLeaderboardAsATrainDepartureBoard/page.php
+++ html/Unres/unresLeaderboardAsATrainDepartureBoard/page.php
@@ -48,7 +48,7 @@
         height: 100%;
         background-image:
         linear-gradient(to bottom, rgba(0,0,0,0.2), transparent),
-        linear-gradient(to left, rgba(0,0,0,0.2), transparent),
+        linear-gradient(to left, rgba(0,0,0,0.2), transparent);
     }
 
   </style>
