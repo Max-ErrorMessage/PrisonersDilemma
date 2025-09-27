67a1f5f33eb3fb918aef8d63ded2d91366bbdecb
GaryFallova <james2004j.a@gmail.com>
2025-09-27 16:50:20 +0100

    lb graph

--- html/Unres/unresLeaderboardAsATrainDepartureBoard/page.php
+++ html/Unres/unresLeaderboardAsATrainDepartureBoard/page.php
@@ -174,7 +174,7 @@ $three = $decks[2];
                 </div>
 
                 <div class="row">
-                    <span>Calling at: Dark Ritual, Mindbreak Trap, Mental Misstep, Barrowgoyf, Vexing Bauble, Orcish Bowmasters, Urza's Saga, Snow-Covered Swamp, Polluted Delta, Thoughtseize, Mox Jet, Black Lotus, Hymn to Tourach, Dauthi Voidwalker, Null Rod, Underground Sea, Urborg, Tomb of Yawgmoth, Sudden Edict, Feed the Swarm</span>
+                    <span>Calling at: </span><span>Dark Ritual, Mindbreak Trap, Mental Misstep, Barrowgoyf, Vexing Bauble, Orcish Bowmasters, Urza's Saga, Snow-Covered Swamp, Polluted Delta, Thoughtseize, Mox Jet, Black Lotus, Hymn to Tourach, Dauthi Voidwalker, Null Rod, Underground Sea, Urborg, Tomb of Yawgmoth, Sudden Edict, Feed the Swarm</span>
                 </div>
 
 
