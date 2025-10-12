f11b226d1df6bd6e72fed037cb0da59bab215ee6
james <james2004j.a@gmail.com>
2025-10-12 16:18:55 +0100

    testing

--- html/Unres/submit_match.php
+++ html/Unres/submit_match.php
@@ -7,9 +7,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (ctype_alnum($name)) {
         $name = escapeshellarg($name);
         $command = "cd /var/www/Unres-Meta/elo && /var/www/Unres-Meta/elo/venv/bin/python update_elo.py $winner_id $loser_id $name 2>&1";
+        echo $command;
         $output2 = shell_exec($command);
 
-        header("Location: Leaderboard.php");
+        //header("Location: Leaderboard.php");
         exit();
     } else {
         header("Location: Match.php?err=alnum");
