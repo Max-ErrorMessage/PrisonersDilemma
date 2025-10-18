fae31af6b262565072190dd1e2300772a43f38ad
GaryFallova <james2004j.a@gmail.com>
2025-10-18 20:48:14 +0100

    lb deck changes

--- html/Unres/leaderboardtest.php
+++ html/Unres/leaderboardtest.php
@@ -294,7 +294,7 @@ foreach ($changes_data as $change_batch) {
 
                                     <?php
                                     if(in_array($deck['id'],$changed_deck_ids)){
-                                        echo '<img src="https://cdn-icons-png.flaticon.com/128/616/616656.png" tooltip="This deck has new changes!">';
+                                        echo '<img style="width:20px;" src="https://cdn-icons-png.flaticon.com/128/616/616656.png" tooltip="This deck has new changes!">';
                                     }
                                     ?>
 
