02d3c8243de42531adac38301248291009f16394
GaryFallova <james2004j.a@gmail.com>
2025-10-25 12:21:26 +0100

    max change

--- html/Unres/submit_match.php
+++ html/Unres/submit_match.php
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (ctype_alnum($name)) {
         $name = escapeshellarg($name);
         $command = "cd /var/www/Unres-Meta/elo && /var/www/Unres-Meta/venv/bin/python3 /var/www/Unres-Meta/elo/update_elo.py $winner_id $loser_id $name";
-        $command2 = "cd /var/www/Unres-Meta/elo && /var/www/Unres-Meta/venv/bin/python3 /var/www/Unres-Meta/elo/update_website_elo.py > /dev/null 2>&1 &";
+        $command2 = "cd /var/www/Unres-Meta/elo && /var/www/Unres-Meta/venv/bin/python3 /var/www/Unres-Meta/elo/update_sheet_elo.py > /dev/null 2>&1 &";
         echo $command;
         $output2 = shell_exec($command);
         $not_needed = shell_exec($command2);
