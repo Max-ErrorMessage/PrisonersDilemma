5792b37047f62cf05a2c4e519cb4f2c016458752
GaryFallova <james2004j.a@gmail.com>
2026-02-09 14:27:41 +0000

    buttons

--- html/Unres/partials/PageButtons.php
+++ html/Unres/partials/PageButtons.php
@@ -1,16 +1,14 @@
 <div class="page-buttons">
-    <a href="index.php" class="pButton">
-        <img src="../images/match.png">
-        <span>Home Page</span>
+    <a href="Match.php" id="p1" class="pButton">
+        <img src="https://cdn-icons-png.flaticon.com/128/9795/9795832.png">
+        <span>Match Submission</span>
     </a>
-
-    <a href="Leaderboard.php" class="pButton">
-        <img src="../images/leaderboard.png">
+    <a href="Leaderboard.php" id="p2" class="pButton">
+        <img src="https://cdn-icons-png.flaticon.com/128/5200/5200866.png">
         <span>Leaderboard</span>
     </a>
-
-    <a href="cards.php" class="pButton">
-        <img src="../images/cards.png">
+    <a href="cards.php" id="p3" class="pButton">
+        <img src="https://cdn-icons-png.flaticon.com/128/6831/6831865.png">
         <span>Top Cards</span>
     </a>
 </div>
