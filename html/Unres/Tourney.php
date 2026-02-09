65f91bbf145ca0fb3f57ee679bf5e0e68fd721df
GaryFallova <james2004j.a@gmail.com>
2026-02-09 14:12:23 +0000

    dir

--- html/Unres/Tourney.php
+++ html/Unres/Tourney.php
@@ -310,7 +310,7 @@ foreach ($changes_data as $change_batch) {
                 
                 <!-- Page Buttons -->
 
-                <?php include $_SERVER['DOCUMENT_ROOT'] . '/partials/PageButtons.php'; ?>
+                <?php include __DIR__ . '/partials/PageButtons.php'; ?>
 
 
                 <!-- Tab Buttons -->
