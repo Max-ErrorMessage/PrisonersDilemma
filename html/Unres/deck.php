e263212b5e6ebbf8366f87d06a30bcaf4ab5d17b
GaryFallova <james2004j.a@gmail.com>
2026-02-13 10:57:30 +0000

    styling

--- html/Unres/deck.php
+++ html/Unres/deck.php
@@ -445,7 +445,8 @@ $tourney_block_display = (!empty($tourney_results)) ? "block" : "none";
                <div id="deck_tourney_results" style="display:<?= $tourney_block_display ?>">
                     </br>
                     <h3 style="text-align:center;">Tournament Results</h3>
-                    <table style="left:20%, width:60%">
+                    </br>
+                    <table style="left:20%; width:60%; position:absolute;">
                         <?php foreach ($tourney_results as $result): ?>
                             <tr onclick=goToTourney('<?= htmlspecialchars($result['tournament_id']) ?>')>
                                 <td>
