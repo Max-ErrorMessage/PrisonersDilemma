e22ce003c7d821a496dd83f72f6cf801fc84d092
james <james2004j.a@gmail.com>
2026-04-12 16:43:37 +0100

    again

--- html/JameGame/JameGameAdminUI.php
+++ html/JameGame/JameGameAdminUI.php
@@ -189,7 +189,7 @@
                 <?php
                 $result = $conn->query("
 Select
-	c.c_id, c.name as c_name, c.cost, a1.name as name, ca.value, s1.name, a2.name as name_1, cona.true_value, s2.name, a3.name as name_2, cona.false_value, s3.name, con.subject, con.type, con.operator, con.value
+	c.c_id, c.name as c_name, c.cost, a1.name as name, ca.value, s1.name as status_1, a2.name as name_1, cona.true_value, s2.name as status_2, a3.name as name_2, cona.false_value, s3.name as status_3, con.subject, con.type, con.operator, con.value
 from Cards c
 inner join CardAbilities ca
 	on c.c_id = ca.c_id
@@ -230,8 +230,8 @@ left join Conditions con
                     } else {
                         $ability = $row['name'] . " " . $row['value'];
 
-                        if ($row['name_2']) {
-                            $ability .= " (" . $row['name_2'] . ")";
+                        if ($row['status_1']) {
+                            $ability .= " (" . $row['status_1'] . ")";
                         }
                     }
 
