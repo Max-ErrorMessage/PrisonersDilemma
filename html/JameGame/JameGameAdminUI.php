bf501c31eebf88f1245139e80f1de60578cfe548
james <james2004j.a@gmail.com>
2026-04-12 15:51:39 +0100

    JameGame UI

---
+++ html/JameGame/JameGameAdminUI.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>Jame Game Admin UI</title>
+	</head>
+	<body>
+		<h1>Jame Game Admin UI</h1>
+		<p>Welcome to the Jame Game Admin UI. Here you can manage your Characters, Cards, Accounts and more.</p>
+	</body>
+</html>
